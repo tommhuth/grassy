7bb09dd10c215085e56ec354d58b9fc8a6a4cc89
Tomm <tomm.huth@hotmail.com>
2021-09-08 11:07:16 +0200

    oops

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -8,9 +8,9 @@ uniform sampler2D gap;
 void main() {
     vec3 top = vec3(.05, .3, .2);
     vec3 bottom = vec3(0., 1., 0.);
-    float u1 = texture2D(cut, vec2(0.,0.));
-    float u2 = texture2D(playerPosition, vec2(0.,0.));
-    float u3 = texture2D(gap, vec2(0.,0.));
+    float u1 = texture2D(cut, vec2(0.,0.)).r;
+    float u2 = texture2D(playerPosition, vec2(0.,0.)).r;
+    float u3 = texture2D(gap, vec2(0.,0.)).r;
 
 
     gl_FragColor = vec4(mix(top, bottom, vPosition.y/2.), clamp(vPosition.y/.25, 0., 1.)); // 
